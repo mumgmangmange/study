8997af829944785819919f6734ac6d7546d77642
mumgmangmange <mumgmangmange@gmail.com>
2025-02-19 01:02:52 +0900

    study: vue.js 개요 및 문법 등등

---
+++ vue-study/ch03/vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    data: function() {
+      return {
+        name: '<h3>망망</h3>',
+        age: 10
+      };
+    }
+  }
+</script>
+<template>
+  <div v-html="name"></div> 이고 {{ age }} 살 입니다.
+</template>
+<style></style>
